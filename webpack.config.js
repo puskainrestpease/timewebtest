c05dc63496aabe0e139e8386429c14cb18c2ed31
Maksos <94627655+puskainrestpease@users.noreply.github.com>
2025-05-25 20:45:29 +0300

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,4 +0,0 @@
-const crypto = require("crypto");
-const cryptoOrigCreateHash = crypto.createHash;
-crypto.createHash = algorithm =>
-  cryptoOrigCreateHash(algorithm === "md4" ? "sha256" : algorithm);
